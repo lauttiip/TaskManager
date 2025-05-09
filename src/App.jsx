0ede88188df12d857f174f3bd730ec878ac6aef1
lautaro perez <lauttiiperez@gmail.com>
2025-05-09 05:02:10 -0300

    configuarion es instalacion de librerias

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import LoginRegister from "./pages/LoginRegister"
+import TaskManager from "./pages/TaskManager"
+import Home from "./pages/Home"  
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login-register" element={<LoginRegister />} />
+        <Route path="/tasks" element={<TaskManager />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Haaaaaaaome</div>
+  )
+}
+
+export default Home
